4aba0190f1696bfae807ca586faa514a96c90cd6
sw8tx <tylerosthoff3@gmail.com>
2026-07-05 16:24:58 +0200

    Use clean hover letter split

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
 import './App.css'
 
+const letters = 'made by Sparkle'.split('')
+
 function App() {
   return (
     <main className="home">
-      <div className="sparkle-stage" aria-label="made by Sparkle">
-        <h1 data-text="made by Sparkle">made by Sparkle</h1>
-        <span className="spark spark-1"></span>
-        <span className="spark spark-2"></span>
-        <span className="spark spark-3"></span>
-        <span className="spark spark-4"></span>
-        <span className="spark spark-5"></span>
-        <span className="spark spark-6"></span>
-        <span className="spark spark-7"></span>
-        <span className="spark spark-8"></span>
-      </div>
+      <h1 className="split-title" aria-label="made by Sparkle">
+        {letters.map((letter, index) => (
+          <span
+            className="letter"
+            style={{ '--i': index }}
+            aria-hidden="true"
+            key={`${letter}-${index}`}
+          >
+            {letter === ' ' ? '\u00a0' : letter}
+          </span>
+        ))}
+      </h1>
     </main>
   )
 }
