a66bb072a8f989d08a2ef11ba8ea15314f6023cb
sw8tx <tylerosthoff3@gmail.com>
2026-07-05 16:30:09 +0200

    Split only hovered word letters

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,18 @@
 import './App.css'
 
-const letters = 'made by Sparkle'.split('')
+const words = ['made', 'by', 'Sparkle']
 
 function App() {
   return (
     <main className="home">
       <h1 className="split-title" aria-label="made by Sparkle">
-        {letters.map((letter, index) => (
-          <span
-            className="letter"
-            style={{ '--i': index }}
-            aria-hidden="true"
-            key={`${letter}-${index}`}
-          >
-            {letter === ' ' ? '\u00a0' : letter}
+        {words.map((word) => (
+          <span className="word" aria-hidden="true" key={word}>
+            {word.split('').map((letter, index) => (
+              <span className="letter" style={{ '--i': index }} key={index}>
+                {letter}
+              </span>
+            ))}
           </span>
         ))}
       </h1>
